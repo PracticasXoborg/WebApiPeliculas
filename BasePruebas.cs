8bbdb2c45a7d0501a7825a754bac4639cecd295d
PracticasXoborg <114386389+PracticasXoborg@users.noreply.github.com>
2022-10-19 13:41:07 +0200

    Add files via upload

---
+++ BasePruebas.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiPeliculasFinal;
+using WebApiPeliculasFinal.Helpers;
+
+namespace PeliculasAPI.Tests
+{
+    public class BasePruebas
+    {
+        //Cada prueba unitaria creará su propia base de datos
+        protected ApplicationDbContext ConstruirContext(string nombreDB)
+        {
+            var opciones = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(nombreDB).Options;
+
+            var dbContext = new ApplicationDbContext(opciones);
+
+            return dbContext;
+
+        }
+
+        protected IMapper ConfigurarAutoMapper()
+        {
+            var config = new MapperConfiguration(options =>
+            {
+                var geometryFactory = NtsGeometryServices.Instance.CreateGeometryFactory(srid: 4326);
+                options.AddProfile(new AutoMapperProfiles(geometryFactory));
+            });
+
+            return config.CreateMapper();
+        }
+    }
+}
